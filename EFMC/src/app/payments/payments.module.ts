2af8bfa26cf6f6a8fc4f864df300eafe5b8a19fb
Noorudins <noorudins@cloudix.io>
2021-12-21 21:46:45 +0530

    payment  menu and receipt details and list added

---
+++ EFMC/src/app/payments/payments.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { SharedModule } from "../shared/shared.module";
+import { CommonModule } from "@angular/common";
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { DataTablesModule } from "angular-datatables";
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { PaymentsComponent } from "./payments.component";
+import { PaymentsRoutingModule } from "./payments.routing.module";
+import { ReceiptsComponent } from "./receipts/receipts.component";
+import { SummaryComponent } from "./summary/summary.component";
+
+
+
+@NgModule({
+  declarations: [
+   PaymentsComponent,
+   ReceiptsComponent,
+   SummaryComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}), //disappear warnings
+    PaymentsRoutingModule,
+    SharedModule,
+    DataTablesModule,
+    BsDatepickerModule.forRoot(),
+    TooltipModule.forRoot()
+    //StoreModule.forFeature(BUILDING_STATE_NAME, buildingsReducer),
+    //EffectsModule.forFeature([BuildingsEffects])
+  ]
+
+})
+export class PaymentsModule{
+
+}
---
+++ EFMC/src/app/payments/receipt-view/receipt-view.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PaymentsService } from 'src/services/payments.service';
+import * as Alert from '../../toster/alert';
+import { Payments } from '../payment.model';
+
+@Component({
+  selector: 'app-receipt-view',
+  templateUrl: './receipt-view.component.html',
+  styleUrls: ['./receipt-view.component.css']
+})
+export class ReceiptViewComponent implements OnInit {
+
+  public flatNo: string;
+  public billNo: string;
+  receiptDetails: Payments[];
+
+  constructor(private route: ActivatedRoute,
+    private routeNav: Router,
+    private paymentsService: PaymentsService) { }
+
+  ngOnInit(): void {
+    this.flatNo = this.route.snapshot.paramMap.get('flatNo');
+    this.fetchReceiptDetails(this.flatNo);
+  }
+
+  private fetchReceiptDetails(flatNo: string){
+
+    this.paymentsService.getReceiptDetail(flatNo).subscribe((receipts: Payments[]) => {
+     if(receipts !== undefined) {
+      this.receiptDetails = receipts;
+     }
+      else {
+        this.routeNav.navigate(['/payments']);
+        Alert.tosterAlert('Data not found !', 'warning')
+      }
+    });
+  }
+
+  onBack(){
+    this.routeNav.navigate(['/payments/receipts']);
+  }
+
+}
